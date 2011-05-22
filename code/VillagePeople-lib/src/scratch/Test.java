5d01d804ddf407f6970569b009c5c7ab1b624ae6
tomasotosolini <tomaso.tosolini@gmail.com>
2011-05-22 19:57:59 +0200

    Added remaining projects files

--- code/VillagePeople-lib/src/scratch/Test.java
+++ code/VillagePeople-lib/src/scratch/Test.java
@@ -9,5 +9,34 @@ package scratch;
  * @author tomaso
  */
 public class Test {
+
+    protected String field1 = "Ciao";
+
+    /**
+     * Get the value of field1
+     *
+     * @return the value of field1
+     */
+    public String getField1() {
+        return field1;
+    }
+
+    /**
+     * Set the value of field1
+     *
+     * @param field1 new value of field1
+     */
+    public void setField1(String field1) {
+        this.field1 = field1;
+    }
+
+    public Test() {
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" + "field1=" + field1 + '}';
+    }
+
     
 }
